58b1d7cfdfcac56f061d9a53f65244b0854257ae
SebasSL <sebslaurens@gmail.com>
2022-01-07 11:08:30 +0100

    add structure

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainRouter from './hoc/MainRouter';
+
+export default function App() {
+  return (
+    <div>
+      <MainRouter />
+    </div>
+  )
+}
---
+++ client/src/hoc/MainRouter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Landing from '../views/Landing/Landing';
+
+export default function MainRouter() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
