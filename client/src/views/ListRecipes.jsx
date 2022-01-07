4823b627ba9953c1ad2f0aa42f891b7875ed50c9
NaiNaiNai17 <fowler.naimah@gmail.com>
2022-01-07 11:54:25 +0100

    added recipe list view

---
+++ client/src/views/ListRecipes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ListRecipes = () => {
+    return (
+        <div>
+          {array.forEach(item => {
+              <li>{array.title}<li/>
+                
+            });}
+        </div>
+    )
+}
+
+export default ListRecipes
